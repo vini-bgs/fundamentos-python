b0eefb4bef534bd40c7825d27654c408ef85c31e
vini-bgs <viniciustheros@gmail.com>
2026-05-31 09:18:07 -0300

    feat: criacao de app2

---
+++ exercicio_02.py
@@ -0,0 +1,9 @@
+# crie um programa onde o usuário digite dois valores e aparece a soma.
+
+print("Informe dois número e eu irei te dizer quanto é a soma deles.")
+num1 = int(input("Número 1: "))
+num2 = int(input("Número 2: "))
+
+soma = num1 + num2
+
+print(f"A soma {num1} + {num2} = {soma}")
