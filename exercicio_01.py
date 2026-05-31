7608617769c035317ec0e90c179b3d9a6d0e5837
vini-bgs <viniciustheros@gmail.com>
2026-05-31 09:13:03 -0300

    feat: adicao de app

---
+++ exercicio_01.py
@@ -0,0 +1,6 @@
+# crie um programa que digita o seu nome e retorna o número de caracteres.
+
+nome = input("Digite o seu nome: ")
+qtde_carac = len(nome)
+
+print(f"Olá, {nome}! O seu nome possui {qtde_carac} caracteres.")
